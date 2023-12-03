a377ea94b7f0358156fa4c25c99cf415e98b2f7a
3dl3rw0lf <arauz.gus@gmail.com>
2023-12-03 10:56:31 -0300

    changes
    Borrado de vite.svg, incorporación de favicon, prueba de ruta de producto

--- src/assets/producto.js
+++ src/assets/producto.js
@@ -1,6 +1,6 @@
 export const product = 
     {
-        img: "./src/assets/img/philips43.webp",
+        img: "../assets/img/philips43.webp",
         title: "Smart TV Philips 43\" FHD Android TV 43PFD6917/77",
         descripcion:  "El Philips 43PFD6917 / 77 es un smart TV compacto y elegante con una pantalla LED de 43 pulgadas y resolución FHD(1920 x 1080p).Su motor de procesamiento optimiza la imagen para ofrecer nitidez, colores ricos y movimientos suaves.Desde videos de YouTube hasta series imperdibles de Netflix, siempre disfrutarás de la mejor experiencia visual posible.",
         precio: "177.999,00",
